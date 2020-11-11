13e5b0e1ec13c8f121282aa69cc2f08d5bbb3c65
Aaron Keller <aaronkeller2021@u.northwestern.edu>
2020-11-11 02:18:36 -0600

    Renamed file

--- link_state_node.py
+++ link_state_node.py
@@ -8,121 +8,225 @@ import json
 class Link_State_Node(Node):
     def __init__(self, id):
         super().__init__(id)
-        self.logging.debug("new node %d" % self.id)
+        self.logging.debug('new node %d' % self.id)
         self.link_seq_nums = {}
         self.link_largest_message_recv = {}
         self.graph = SimulatorGraph(0)
 
+    def check_exists(self, node1, node2):
+        return (node1 in self.graph.adj_list and node2 in self.graph.adj_list[node1])
+    
+    def increment_seq_num(self, src, dst):
+        # check if src and dst are in link_seq_num
+        if src not in self.link_seq_nums:
+            self.link_seq_nums[src] = {}
+        if dst not in self.link_seq_nums:
+            self.link_seq_nums[dst] = {}
+        
+        # actually do the incrementing
+        if dst not in self.link_seq_nums[src]:
+            self.link_seq_nums[src][dst] = 1 
+        else:
+            self.link_seq_nums[src][dst] += 1
+        if src not in self.link_seq_nums[dst]:
+            self.link_seq_nums[dst][src] = 1 
+        else:
+            self.link_seq_nums[dst][src] += 1
+
+    def update_seq_num(self, src, dst, new_seq_num):
+        if src not in self.link_seq_nums:
+            self.link_seq_nums[src] = {}
+        if dst not in self.link_seq_nums:
+            self.link_seq_nums[dst] = {}
+        self.link_seq_nums[src][dst] = new_seq_num
+        self.link_seq_nums[dst][src] = new_seq_num   
+        
+    def update_lmr(self, src, dst, msg):
+        #msg = json.loads(msg_json)
+        #if self.id==20:
+            #print('Updating node {}'s lmr to {}-{}-{}'.format(self.id, src, dst, msg))
+        if src not in self.link_largest_message_recv:
+            #self.link_largest_message_recv.update({src: {dst: msg}})
+            self.link_largest_message_recv[src] = {}
+        if dst not in self.link_largest_message_recv:
+            #self.link_largest_message_recv.update({dst: {src: msg}})
+            self.link_largest_message_recv[dst] = {}
+        self.link_largest_message_recv[dst][src] = msg
+        self.link_largest_message_recv[src][dst] = msg
+        
+        #if self.id==20:
+         #   print("Updated node {}'s lmr to: {}".format(self.id, msg))
+            
+    
+    def process_message(self, msg):
+        #print(type(msg))
+        #msg_dict = json.loads(msg)
+        msg_dict = msg
+        #print(type(msg_dict))
+        #print(msg_dict)
+        
+        #if isinstance(msg_dict, str):
+         #   msg_dict = json.loads(msg_dict)
+        src = msg_dict['src']
+        src = int(src)
+        dst = msg_dict['dst']
+        dst = int(dst)
+        latency = msg_dict['cost']
+        latency = int(latency)
+        seq = msg_dict['seq']
+        seq = int(seq)
+        
+        if latency == -1:
+            if self.check_exists(src, dst) and seq > self.link_seq_nums[src][dst]:
+                self.graph.remove_edge(src, dst) # remove edge checks if it exists
+                self.update_seq_num(src, dst, seq)
+                if src == self.id:
+                    self.neighbors.remove(dst)
+                if dst == self.id:
+                    self.neighbors.remove(src)
+                self.update_lmr(src, dst, msg)
+                return True
+            else:
+                if src in self.link_largest_message_recv and dst not in self.link_largest_message_recv[src]:
+                    #print(self.id, msg, self.link_largest_message_recv)
+                    self.update_lmr(src, dst, msg)
+                elif dst in self.link_largest_message_recv and src not in self.link_largest_message_recv[dst]:
+                    #print(self.id, msg, self.link_largest_message_recv)
+                    self.update_lmr(src, dst, msg)
+                return self.link_largest_message_recv[src][dst] 
+        # do this if latency is non-negative
+        if not self.check_exists(src, dst):
+            if src in self.link_largest_message_recv:
+                if dst in self.link_largest_message_recv[src] and self.link_largest_message_recv[src][dst]["cost"] == -1:
+                    return self.link_largest_message_recv[src][dst]
+            if dst == self.id:
+                self.neighbors.append(src)
+            if src == self.id:
+                self.neighbors.append(dst)
+            self.graph.add_edge(src, dst, latency)
+            self.update_seq_num(src, dst, seq)
+            self.update_lmr(src, dst, msg)
+            return True
+        elif seq > self.link_seq_nums[src][dst]: # and latency < self.graph.adj_list[src][1]:
+            self.graph.add_edge(src, dst, latency)
+            self.update_seq_num(src, dst, seq)
+            self.update_lmr(src, dst, msg)
+            return True
+        #print(self.id, src, dst, self.link_largest_message_recv)
+        #print(self.link_largest_message_recv[src][dst])
+        return self.link_largest_message_recv[src][dst]
+
     # Return a string
     def __str__(self):
         neighbor_str = self.graph.get_neighbors(self.id)
-        return ""
+        return ''
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        if self.id == 4:
-            #self.graph.print_graph()
-            pass
         new_neighbor = False
         if latency == -1 and neighbor in self.neighbors:
             self.neighbors.remove(neighbor)
             self.graph.remove_edge(self.id, neighbor)
         else:
             new_neighbor = neighbor not in self.graph.adj_list
-
-            self.neighbors.append(neighbor)
-            #print(self.id, neighbor)
-            #print("link_has_been_updated: adding edge ({}, {})".format(self.id, neighbor))
+            if neighbor not in self.neighbors:
+                self.neighbors.append(neighbor)
             self.graph.add_edge(self.id, neighbor, latency)
-            #print(self.graph.print_graph())
-            # new_node_message = {"graph": self.graph.adj_list, "seq_nums": self.link_seq_nums}
-            # new_node_message_json = json.dumps(new_node_message)
-            # longest_message_recv_json = json.dumps(self.link_largest_message_recv)
-            #self.send_to_neighbor(neighbor, new_node_message_json)
-            # {src, dst, cost, seq/time}
         
         # might have to update largest message receive idk
-        if neighbor not in self.link_seq_nums:
-            self.link_seq_nums[neighbor] = 1
-        else:
-            self.link_seq_nums[neighbor] += 1
         
+        self.increment_seq_num(self.id, neighbor)
+        
+        #print(self.id, neighbor, self.link_seq_nums)
+        message = {'src': self.id, 'dst': neighbor, 'cost': latency, 'seq': self.link_seq_nums[self.id][neighbor]}
+        json_message = json.dumps(message)
+        self.update_lmr(self.id, neighbor, message)
+
         #Pack into JSON
-        if new_neighbor:
-            message = {"src": self.id, "dst": neighbor, "cost": latency, "seq": self.link_seq_nums[neighbor], "largest_messages": self.link_largest_message_recv}
+        if new_neighbor and self.link_largest_message_recv != {}:
+            message = {'src': self.id, 'dst': neighbor, 'cost': latency, 'seq': self.link_seq_nums[self.id][neighbor], 'largest_messages': self.link_largest_message_recv}
             json_message = json.dumps(message)
             self.send_to_neighbor(neighbor, json_message)
+        # send new link data to all neighbors
+        
         for n in self.neighbors:
             if n != neighbor:
-                message = {"src": self.id, "dst": neighbor, "cost": latency, "seq": self.link_seq_nums[neighbor]}
-                json_message = json.dumps(message)
                 self.send_to_neighbor(n, json_message)
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
+        # if self.id == 10:
+        #     print(m)
         message = json.loads(m)
-        # if "graph" in message and "seq_nums" in message:
-        #     temp_adj_list = message["graph"]
-        #     for node1, val in temp_adj_list.items():
-        #         for node2, cost in val.items(): # probably causing issues, adding cost that's higher than our lowest cost
-        #             self.graph.add_edge(int(node1), int(node2), int(cost))
-        #     temp_link_seq_nums = message["seq_nums"]
-        #     #self.link_seq_nums = message["seq_nums"]
-        #     print(self.id, self.graph.adj_list)
-        #     for node, seq_num in temp_link_seq_nums.items():
-        #         if node in self.link_seq_nums:
-        #             if seq_num > self.link_seq_nums[node]:
-        #                 self.link_seq_nums[node] = seq_num
-        #                 message = {"src": self.id, "dst": self.id, "cost": latency, "seq": self.link_seq_nums[neighbor]}
-        #                 json_message = json.dumps(message)
-        #                 self.link_largest_message_recv = json_message
-        #         else:
-        #             self.link_seq_nums[node] = seq_num
-        #     return
-        
         # figure out which largest messages are relevant to us
-        if "largest_messages" in message:
-            msg_dict = message["largest_messages"]
-            for msg in msg_dict.values():
-                new_dict = json.loads(msg)
-                src = new_dict["src"]
-                dst = new_dict["dst"]
-                latency = new_dict["cost"]
-                seq = new_dict["seq"]
-                if src not in self.graph.adj_list:
-                    self.graph.add_edge(src, dst, latency)
-                    self.link_seq_nums[src] = seq
-                    self.link_largest_message_recv[src] = msg
-                elif seq > self.link_seq_nums[src]:# and latency < self.graph.adj_list[src][1]:
-                    self.graph.add_edge(src, dst, latency)
-                    self.link_seq_nums[src] = seq
-                    self.link_largest_message_recv[src] = msg
+        #print(self.id, message)
+        src = int(message['src']) # who we received this message from
+        dst = int(message['dst']) # the node the sender is connected to
+        latency = int(message['cost'])
+        seq = int(message['seq'])
+        updated_response = {}
+        forward_response = {}
+        first_update = self.process_message(message)
+        if 'largest_messages' in message:
+            node_dict = message['largest_messages']
+            for node1 in node_dict:
+                for node2 in node_dict[node1]:
+                    #msg_json = json.dumps(node_dict[node1][node2])
+                    #print("msg json: ", msg_json)
+                    #is_new = self.process_message(json.dumps(node_dict[node1][node2]))
+                    is_new = self.process_message(node_dict[node1][node2])
+                    if is_new != True:
+                        #print(self.id, message, updated_response)
+                        #self.graph.print_graph()
+                        if int(node1) not in updated_response:
+                            updated_response[int(node1)] = {}
+                        updated_response[int(node1)][int(node2)] = is_new
+                    else:
+                        if int(node1) not in forward_response:
+                            forward_response[int(node1)] = {}
+                        forward_response[int(node1)][int(node2)] = node_dict[node1][node2]
+        if first_update != True or updated_response != {}: # have to resend something
+            #print(updated_response)
+            if first_update != True:
+                first_update_obj = first_update #json.loads(first_update)
+                msg = {'src': src, 'dst': dst, 'cost': first_update_obj['cost'], 'seq': first_update_obj['seq']}
+                json_msg = json.dumps(msg)
+            if first_update == True: # just resend data we received, must have to send other data
+                msg = {'src': src, 'dst': dst, 'cost': latency, 'seq': seq, 'largest_messages': updated_response}
+                json_msg = json.dumps(msg)
+                # if first_update == True:
+                #     for neighbor in self.neighbors:
+                #         if neighbor != src: 
+                #             self.send_to_neighbor(neighbor, m)
+            elif updated_response != {}:
+                first_update_obj = first_update # json.loads(first_update)
+                msg = {'src': src, 'dst': dst, 'cost': first_update_obj['cost'], 'seq': first_update_obj['seq'], 'largest_messages': updated_response}
+                json_msg = json.dumps(msg)
+            self.send_to_neighbor(src, json_msg) # send some variant of the above messages
         
-        src = message["src"]
-        dst = message["dst"]
-        latency = message["cost"]
-        seq = message["seq"]
-        print('id', self.id, "src", src, "dst", dst, "seq", seq, self.link_seq_nums)
-        
-        if src not in self.link_seq_nums or seq > self.link_seq_nums[src] or src not in self.link_largest_message_recv:
-            self.link_seq_nums[src] = seq
-            self.link_largest_message_recv[src] = m
-            if latency == -1 and src in self.neighbors:
-                self.neighbors.remove(src)
-                self.graph.remove_edge(src, dst)
+        if first_update == True or forward_response != {}: 
+            if forward_response != {}:
+                msg = {'src': src, 'dst': dst, 'cost': latency, 'seq': seq, 'largest_messages': forward_response}
+                json_msg = json.dumps(msg)
+                for neighbor in self.neighbors:
+                    if neighbor != src: 
+                        self.send_to_neighbor(neighbor, json_msg)
             else:
-                #check is new node added and new node is adjacent 
-                if self.id == dst and src not in self.neighbors:
-                    self.neighbors.append(src)
-                #print("process_incoming_routing_message: adding edge ({}, {})".format(src, dst))
-                self.graph.add_edge(src, dst, latency)
-            #send to all neighbors except original sender
-            for n in self.neighbors:
-                if n != src:
-                    self.send_to_neighbor(n, m)
-        else:
-            #send largest message received back to sender
-            self.send_to_neighbor(src, self.link_largest_message_recv[src])
+                msg = {'src': src, 'dst': dst, 'cost': latency, 'seq': seq}
+                json_msg = json.dumps(msg)
+                for neighbor in self.neighbors:
+                    if neighbor != src: 
+                        self.send_to_neighbor(neighbor, json_msg)
+        
+        
+        #if self.id == 2:
+         #   print(self.id, self.neighbors)
+        #if first_update == True:
+            # some information is new, send the original message
+            # for neighbor in self.neighbors:
+            #     if neighbor != src: 
+            #         self.send_to_neighbor(neighbor, m)
+
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
